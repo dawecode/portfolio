3f1e495ef9cfd6c3e2fc77eb163127943a2da47c
Catherine Dawe <dawe.cm@gmail.com>
2021-04-28 18:26:43 -0400

    add skills layout

--- portfolio/src/components/Skills.js
+++ portfolio/src/components/Skills.js
@@ -4,6 +4,29 @@ export default function Skills() {
   return (
     <div className="skills child">
       <h1> Skills</h1>
+      <div className= "skills__list">
+      <ul>
+        <li>cool</li>
+        <li>cool</li>
+        <li>cool</li>
+        <li>cool</li>
+        <li>cool</li>
+      </ul>
+      <ul>
+        <li>cool</li>
+        <li>cool</li>
+        <li>cool</li>
+        <li>cool</li>
+        <li>cool</li>
+      </ul>
+      <ul>
+        <li>cool</li>
+        <li>cool</li>
+        <li>cool</li>
+        <li>cool</li>
+        <li>cool</li>
+      </ul>
+      </div>
     </div>
-  )
+  );
 }
