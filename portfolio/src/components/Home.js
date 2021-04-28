8238794d664ea3d4eb495a11af6e73aeb1f2d593
Catherine Dawe <dawe.cm@gmail.com>
2021-04-27 20:31:55 -0400

    add sass and styling

---
+++ portfolio/src/components/Home.js
@@ -0,0 +1,12 @@
+import React from 'react'
+
+export default function Home() {
+  return (
+      <div className="home">
+      <header className="App-header">
+        <h1> Catherine Dawe </h1>
+       <p> Full Stack Web Developer </p>
+      </header>
+    </div>
+  )
+}
