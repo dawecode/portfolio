41c3c6e0026e5c414a1c259d043c1bb24dd5d2b1
Catherine Dawe <dawe.cm@gmail.com>
2021-04-28 22:08:22 -0400

    add project page

--- portfolio/src/components/Projects.js
+++ portfolio/src/components/Projects.js
@@ -3,7 +3,21 @@ import React from 'react'
 export default function Projects() {
   return (
     <div className="projects child">
-       what ever dude 
+      <h1>Projects</h1>
+      <div className="projects__list">
+        <div>
+          <h3>Pawly</h3>
+          <h6> Pawly is a coool dude that needs friendz for his dog</h6>
+        </div>
+        <div>
+          <h3>Scheduler</h3>
+          <h6> schedule me</h6>
+        </div>
+        <div>
+          <h3>Tweeter</h3>
+          <h6> tweet me</h6>
+        </div>
+      </div>
     </div>
-  )
+  );
 }
--- portfolio/src/components/Skills.js
+++ portfolio/src/components/Skills.js
@@ -13,22 +13,22 @@ export default function Skills() {
         <h3>SQL</h3>
         <h3>Ruby</h3>
       </div>
-      <ul>
+      <div>
         <h3>NodeJS</h3>
         <h3>React</h3>
         <h3>Express</h3>
         <h3>jQuery</h3>
         <h3>Ajax</h3>
         <h3>Rails</h3>
-      </ul>
-      <ul>
+      </div>
+      <div>
         <h3>PostgreSQL</h3>
         <h3>MongoDB</h3>
         <h3>Mocha/Chai</h3>
         <h3>Cypress</h3>
         <h3>Jest</h3>
         <h3>Rspec</h3>
-      </ul>
+      </div>
       </div>
     </div>
   );
