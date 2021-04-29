2bb66d0621b577ed3a245324b821a716ddc5daca
Catherine Dawe <dawe.cm@gmail.com>
2021-04-28 21:03:15 -0400

    add skills

--- portfolio/src/components/Skills.js
+++ portfolio/src/components/Skills.js
@@ -5,26 +5,29 @@ export default function Skills() {
     <div className="skills child">
       <h1> Skills</h1>
       <div className= "skills__list">
+      <div>
+        <h3>JavaScript</h3>
+        <h3>HTML</h3>
+        <h3>CSS</h3>
+        <h3>SASS/SCSS</h3>
+        <h3>SQL</h3>
+        <h3>Ruby</h3>
+      </div>
       <ul>
-        <li>cool</li>
-        <li>cool</li>
-        <li>cool</li>
-        <li>cool</li>
-        <li>cool</li>
+        <h3>NodeJS</h3>
+        <h3>React</h3>
+        <h3>Express</h3>
+        <h3>jQuery</h3>
+        <h3>Ajax</h3>
+        <h3>Rails</h3>
       </ul>
       <ul>
-        <li>cool</li>
-        <li>cool</li>
-        <li>cool</li>
-        <li>cool</li>
-        <li>cool</li>
-      </ul>
-      <ul>
-        <li>cool</li>
-        <li>cool</li>
-        <li>cool</li>
-        <li>cool</li>
-        <li>cool</li>
+        <h3>PostgreSQL</h3>
+        <h3>MongoDB</h3>
+        <h3>Mocha/Chai</h3>
+        <h3>Cypress</h3>
+        <h3>Jest</h3>
+        <h3>Rspec</h3>
       </ul>
       </div>
     </div>
